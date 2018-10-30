e5019793226cacdb7b9061cd0a1455b84d213dca
Gavri Kepets <gkepets@gmail.com>
2018-10-30 08:26:21 +0200

    added player function

--- app.js
+++ app.js
@@ -11,31 +11,47 @@ app.use('/client', express.static(__dirname + '/client'))
 s.listen(2000);
 
 var socketList = {};
+var playerList = {};
+
+var Player = function(id){
+    var self = {
+        x:250,
+        y:250,
+        id:id,
+        number: "" + Math.floor(10 * Math.random()),
+        right:false,
+        left:false,
+        up:false,
+        down:false
+    }
+    return self;
+}
 
 var io = require('socket.io')(s, {});
 io.sockets.on('connection', function (socket){
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 * Math.random());
+    
     socketList[socket.id] = socket;
+
+    playerList[socket.id] = Player(socket.id);
 
     socket.on('disconnect', function(){
         delete socketList[socket.id];
+        delete playerList[socket.id];
     })
 
 })
 
 setInterval(function(){
     var pack = [];
-    for(var i in socketList){
-        var socket = socketList[i];
-        socket.x++;
-        socket.y++;
+    for(var i in playerList){
+        var player = playerList[i];
+        player.x++;
+        player.y++;
         pack.push({
-            x:socket.x,
-            y:socket.y,
-            number:socket.number
+            x:player.x,
+            y:player.y,
+            number:player.number
         })
     }
     for(var i in socketList){
